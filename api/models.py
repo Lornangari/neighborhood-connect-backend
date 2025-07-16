3cbcd06a395c729bb0187cc051c2feef9684f99c
Lornangari <lornangari@gmail.com>
2025-07-16 20:55:42 +0300

    Created Neighborhood model and linked to custom User model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    neighborhood = models.CharField(max_length=100, blank=True, null=True)
+    neighborhood = models.ForeignKey(
+        'Neighborhood', on_delete=models.SET_NULL, null=True, blank=True, related_name='residents'
+    )
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.name
