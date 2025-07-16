a964a8e9d4509ad74297490544fcce528982c02c
Lornangari <lornangari@gmail.com>
2025-07-16 20:58:59 +0300

    Created Post model linked to User and Neighborhood

--- api/models.py
+++ api/models.py
@@ -15,3 +15,15 @@ class Neighborhood(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.author.username}"
+
